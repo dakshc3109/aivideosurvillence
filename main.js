e07760a160a79e2c768a40114cbb7df2a91d0998
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-11-25 17:29:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 //main.js
+var status = "";
+var video = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(480 ,320);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 320);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Detecting Objects"
+}
+
+function modelLoaded(){
+    console.log("model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
