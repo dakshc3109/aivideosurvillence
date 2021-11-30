daf5f424c524f246753befc9706c194455b1dca4
Daksh Chaudhary <78908644+dakshc3109@users.noreply.github.com>
2021-11-30 16:42:39 +0530

    completed js code
    Completed the project

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //main.js
+objects = [];
 var status = "";
 var video = "";
 
@@ -14,11 +15,24 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 480, 320);
+    if(status != ""){
+        objectDetector.detect(video, gotResults);
+        for(i = 0; i < objects.length ; i++){
+            fill("#ff0000");
+            percentage = floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percentage + "%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("status").innerHTML = "Objects Detected";
+            document.getElementById("name-of-objects").innerHTML = "Number of objects detected "+objects.length;
+        }
+    }
 }
 
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Detecting Objects"
+    document.getElementById("status").innerHTML = "Detecting Objects";
 }
 
 function modelLoaded(){
@@ -27,4 +41,15 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.error();
+    }
+    else{   
+         //console.log(results);
+        objects = results;
+        console.log(objects);
+    }
 }
